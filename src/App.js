71bcde297f7dfdebb5b58f4b11a4847b9937e719
djeriko <djeriko>
2019-10-01 19:46:27 +0900

    mount unmount in useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "./useForm";
+import { Hello } from "./Hello";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: ""
+  });
+
+  // const [showHello, setShowHello] = useState(true);
+
+  useEffect(() => {
+    console.log("render");
+
+    return () => {
+      console.log("unmount");
+    };
+  }, [values.email]);
 
   return (
     <div>
-      <input name="email" value={values.email} onChange={handleChange} />
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle</button> */}
+      {/* {showHello && <Hello />} */}
+      <input
+        name="email"
+        placeholder="Enter email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        name="firstName"
+        placeholder="Enter name"
+        value={values.firstName}
+        onChange={handleChange}
+      />
       <input
         type="password"
         name="password"
+        placeholder="Enter password"
         value={values.password}
         onChange={handleChange}
       />
